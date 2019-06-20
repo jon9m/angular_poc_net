a4556a8339427d2ca9ef17bbed4748445e22540c
jon <manojam79@gmail.com>
2019-06-20 17:23:38 +1000

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DetailscardComponent } from './detailscard/detailscard.component';
+import { ResidentialComponent } from './residential/residential.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadcrumbComponent,
+    ProfileComponent,
+    DetailscardComponent,
+    ResidentialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,126 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  itemList: Object;
+  itemKeys: string[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemList = {
+      'Address': [
+        {
+          path: 'recidential',
+          description: 'Recidential'
+        },
+        {
+          path: 'pobox',
+          description: 'PO Box'
+        }
+      ],
+      'Contact details': [
+        {
+          path: 'phone',
+          description: 'Phone'
+        },
+        {
+          path: 'email',
+          description: 'Email'
+        }
+      ],
+      'Contact preference': [
+        {
+          path: 'sms',
+          description: 'SMS'
+        },
+        {
+          path: 'cp/phone',
+          description: 'phone'
+        },
+        {
+          path: 'cp/email',
+          description: 'Email'
+        }
+      ],
+      'Digital wallet management': [
+        {
+          path: 'adddocument',
+          description: 'Add document'
+        },
+        {
+          path: 'removedocument',
+          description: 'Remove document'
+        },
+        {
+          path: 'removemobiledevice',
+          description: 'Remove mobile device'
+        }
+      ],
+      'Licenses': [
+        {
+          path: 'licence',
+          description: 'Car'
+        },
+        {
+          path: 'licence',
+          description: 'Boat'
+        },
+        {
+          path: 'licence',
+          description: 'Helicopter'
+        }
+      ],
+      'Registrations': [
+        {
+          path: 'registrations',
+          description: 'Car'
+        },
+        {
+          path: 'registrations',
+          description: 'Boat'
+        },
+        {
+          path: 'registrations',
+          description: 'Helicopter'
+        }
+      ],
+      'Infringements': [
+        {
+          path: 'infringements',
+          description: 'Speeding fine'
+        },
+        {
+          path: 'infringements',
+          description: 'Speeding fine 2'
+        },
+        {
+          path: 'infringements',
+          description: 'Speeding fine 3'
+        }
+      ],
+      'Other products': [
+        {
+          path: 'otherproducts',
+          description: 'Product x'
+        },
+        {
+          path: 'otherproducts',
+          description: 'Product B'
+        },
+        {
+          path: 'otherproducts',
+          description: 'Product P'
+        }
+      ]
+    };
+
+    this.itemKeys = Object.keys(this.itemList);
+  }
+
+}
